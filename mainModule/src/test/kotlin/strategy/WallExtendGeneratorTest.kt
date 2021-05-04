5d732d47765a1dbaa53ac1d5d02926ea0409920a
iaia <iaia72160@gmail.com>
2021-05-04 23:32:19 +0900

    fix

--- mainModule/src/test/kotlin/strategy/WallExtendGeneratorTest.kt
+++ mainModule/src/test/kotlin/strategy/WallExtendGeneratorTest.kt
@@ -1,7 +1,15 @@
 package strategy
 
+import Generator
 import io.kotest.core.spec.style.DescribeSpec
 
 internal class WallExtendGeneratorTest : DescribeSpec({
+    describe("5x5") {
+        val generator: Generator = WallExtendGenerator(5, 5)
 
+        it("テスト") {
+            val maze = generator.generate()
+            maze.output()
+        }
+    }
 })
--- mazeGenerator/src/main/kotlin/strategy/WallExtendGenerator.kt
+++ mazeGenerator/src/main/kotlin/strategy/WallExtendGenerator.kt
@@ -13,6 +13,8 @@ class WallExtendGenerator(
     private val extendDirection: Array<Direction> =
         arrayOf(Direction.LEFT, Direction.BELOW, Direction.ABOVE, Direction.RIGHT)
 
+    private val building: MutableList<XY> = mutableListOf()
+
     override fun buildMap() {
         setStartAndGoal()
         fillMap()
@@ -20,6 +22,7 @@ class WallExtendGenerator(
 
         while (selectedXY.isNotEmpty()) {
             val xy = selectedXY.random()
+            building.clear()
             if (cells.here(xy) is Cell.Floor) {
                 extendWall(xy)
             }
@@ -41,39 +44,54 @@ class WallExtendGenerator(
     }
 
     private fun extendWall(
-        xy: XY,
-        building: MutableList<XY> = mutableListOf(),
+        startXY: XY,
         except: Array<Direction> = emptyArray()
     ) {
-        val cell0 = cells.here(xy)
-        if (cell0 !is Cell.Floor) {
-            return
+        // 開始位置をまず壁にする
+        val cell0 = cells.here(startXY)
+        if (cell0 == null || cell0 is Cell.Start || cell0 is Cell.Goal) {
+            throw Exception()
         }
-
-        val direction = randomDirection(except) ?: return
-        val cell1 = cells.here(direction.calculate(xy))
-        if (cell1 == null || building.contains(cell1.xy)) {
-            extendWall(xy, building, except + direction)
-            return
-        }
-        val cell2 = cells.here(direction.calculate(cell1.xy))
-        if (cell2 == null || building.contains(cell2.xy)) {
-            extendWall(xy, building, except + direction)
-            return
-        }
-
-        if (cell1 is Cell.Start || cell1 is Cell.Goal || cell2 is Cell.Start || cell2 is Cell.Goal) {
-            extendWall(xy, building, except + direction)
-            return
-        }
-
-        if (cell1 is Cell.Floor) {
+        if (cell0 is Cell.Floor) {
             cells.add(Cell.Wall(cell0.xy))
             building.add(cell0.xy)
-            cells.add(Cell.Wall(cell1.xy))
-            building.add(cell1.xy)
         }
-        extendWall(cell2.xy)
+        // 上下左右のいずれかの座標を取り出す ただし、上下左右壁が伸ばせない場合は終了する
+        val direction = randomDirection(except) ?: return
+        val cell1 = cells.here(direction.calculate(startXY))
+        // その座標が通路なら、更に1個その方向の座標を取り出す
+        if (cell1 is Cell.Floor) {
+            when (val cell2 = cells.here(direction.calculate(cell1.xy))) {
+                is Cell.Start, is Cell.Goal -> {
+                    extendWall(startXY, except)
+                }
+                is Cell.Wall -> {
+                    if (building.contains(cell2.xy)) {
+                        // その座標が壁伸ばし中の壁ならその方向には建設せずに、別の座標を選ぶ (つまり戻って別座標に向かって壁を建設する)
+                        extendWall(startXY, except + direction)
+                    } else {
+                        // その座標が既存の壁なら、1個目まで建設して終了する
+                        cells.add(Cell.Wall(cell1.xy))
+                        building.add(cell1.xy)
+                        return
+                    }
+                }
+                is Cell.Floor -> {
+                    // 床なら壁を2個建設して更に末端から延長する
+                    cells.add(Cell.Wall(cell1.xy))
+                    building.add(cell1.xy)
+                    cells.add(Cell.Wall(cell2.xy))
+                    building.add(cell2.xy)
+                    extendWall(cell2.xy)
+                }
+                else -> {
+                    throw Exception()
+                }
+            }
+        } else {
+            // 床以外なら別の方向で建設をする
+            extendWall(startXY, except + direction)
+        }
     }
 
     private fun fillMap() {
